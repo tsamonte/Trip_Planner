7b90e99c975a78da3c2ad6cb782828995a1d0048
Tristan Samonte <tsamonte23@gmail.com>
2019-04-21 00:56:51 -0700

    added matrix and directions functions

--- getDirections.py
+++ getDirections.py
@@ -1,2 +1,14 @@
-def getDistanceMatrix(gmaps: 'googlemaps Client', placeList: list):
-    asdf
+def getDistanceMatrix(gmaps: 'googlemaps Client', origin: 'latLong', destination: 'latLong'):
+    matrix = gmaps.distance_matrix(origins = origin,
+                                   destinations = destination,
+                                   mode = 'driving',
+                                   units = 'imperial')
+
+    return matrix
+
+def getDirections(gmaps: 'googlemaps Client', origin: 'latLong', destination: 'latLong'):
+    directions = gmaps.directions(origin = origin,
+                                  destination = destination,
+                                  mode = 'driving',
+                                  units = 'imperial')
+    return directions
