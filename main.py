d50c78a3acc17d94df17062181b2b11a5c6858dd
Tristan Samonte <tsamonte23@gmail.com>
2019-04-20 22:59:36 -0700

    removed key

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import googlemaps
 import getPlaces
+import getDirections
 
 def main():
-    gmaps = googlemaps.Client(key='AIzaSyA4j2DBPo0Lkk0q1p8zIjGF4PFdTImfCFI')
+    keyFile = open('googlemapsKey.txt', 'r')
+    key = keyFile.read()
+    keyFile.close()
+
+    gmaps = googlemaps.Client(key=key)
     # place = input("Please input your destination: ")
     # start = input("Please input your starting time: ")
     place = "7707 Aldea Ave, Van Nuys, CA"
@@ -10,9 +15,11 @@ def main():
     time = start.split(':') # time[0] = hr; time[1] = min
 
     placeDict = {}
+    placeNames = [] # needed to keep order of destinations
 
     initialPlace = getPlaces.getInitialPlace(gmaps, place)
     placeDict[initialPlace[0]] = initialPlace[2]
+    placeNames.append(initialPlace[0])
     print(initialPlace)
 
     nextPlace = initialPlace
@@ -25,9 +32,12 @@ def main():
             keyword = 'tourist attractions'
             type = None
 
-        nextPlace = getPlaces.getNearPlace(gmaps, nextPlace[2], keyword, type, placeDict)
+        nextPlace = getPlaces.getNearPlace(gmaps, nextPlace[2], keyword, type, placeNames)
         placeDict[nextPlace[0]] = nextPlace[2]
+        placeNames.append(nextPlace[0])
         print(nextPlace)
+
+    getDirections.getDistanceMatrix(gmaps, placeNames, placeDict)
 
 
 if __name__ == "__main__":
