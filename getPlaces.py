023819b11f199a46c6e784b06c1629cd2300ed41
Tristan Samonte <tsamonte23@gmail.com>
2019-04-21 02:38:52 -0700

    added docstrings

--- getPlaces.py
+++ getPlaces.py
@@ -1,4 +1,18 @@
+# getPlaces.py
+# Makes use of the Places API within the Googlemaps API.
+# Functions in this module handle locating places located near specified addresses.
+
 def getInitialPlace(gmaps: 'gmapsClient', readableAddress: str):
+    """Returns an initial place of interest located near a specified location
+
+    :param gmaps: The googlemaps client object, necessary to call any of the googlemaps API functions
+    :type gmaps: googlemaps.Client
+
+    :param readableAddress: The human-readable version of an address, i.e. "123 Bottle Street, Irvine, CA"
+    :type readableAddress: str
+
+    :return: list of necessary information for the chosen place
+    """
     geocode = gmaps.geocode(address=readableAddress)
     latLong = str(geocode[0]["geometry"]["location"]["lat"]) + "," + str(geocode[0]["geometry"]["location"]["lng"])
     places = gmaps.places(query = "tourist attractions",
@@ -20,6 +34,25 @@ def getInitialPlace(gmaps: 'gmapsClient', readableAddress: str):
 
 
 def getNearPlace(gmaps: 'gmapsClient', location: 'latLng',  keyword: str, type: 'googlemaps type', placeNames: list):
+    """Returns a place near the previous location within a smaller radius
+
+    :param gmaps: The googlemaps client object, necessary to call any of the googlemaps API functions
+    :type gmaps: googlemaps.Client
+
+    :param location: The latitude+longitude location of the previous location
+    :type location: str
+
+    :param keyword: A word to query for the next location
+    :type keyword: str
+
+    :param type: Restricts results to places of a specific type; to use in call to places_nearby
+    :type type: str
+
+    :param placeNames: A list containing the previously chosen places
+    :type placeNames: list
+
+    :return: list of necessary information for the chosen place
+    """
     places = gmaps.places_nearby(location = location,
                                     # radius = 4828, # 2 miles
                                     keyword = keyword,
